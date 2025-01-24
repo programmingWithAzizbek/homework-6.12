56f178cc4c101a2bb5f622e86884cb8e31c8ffda
AZIZBEK AKBAROV <akbarovazizbekoybekovich3@gmail.com>
2025-01-24 16:17:19 +0500

    first commit

--- src/pages/ProductsDetails.jsx
+++ src/pages/ProductsDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, NavLink } from "react-router-dom";
 import Header from "../components/Header";
-import { backendAPI } from "../Axios";
+import { backendAPI } from "../axios";
 
 function ProductsDetails() {
   const [products, setProducts] = useState([]);
